e004502d3c79e0756927360adb08789a8a0899d8
snow300190 <snow.erofeevivan@yandex.ru>
2016-02-03 20:06:15 +0300

    Pagging with parammeters

--- store/views.py
+++ store/views.py
@@ -4,20 +4,27 @@ from django.core.paginator import Paginator
 
 
 # Create your views here.
-def main_page(request, group = 0, page = 1):
+def main_page(request):
     args = {}
-    if group != 0 and group != '0':
-        all_good = Good.objects.filter(good_group_id=group)
-        args["current_group"] = Group.objects.get(id=group)
-    else:
-        all_good = Good.objects.all()
-        args["current_group"] = group
-
+    all_good = Good.objects.all()
+    args["current_group"] = None
+    page = request.GET.get('p')
     filtered_goods = Paginator(all_good,6)
-    args["goods"] = filtered_goods.page(page)
+    args["goods"] = filtered_goods.page(page if page != None else 1)
     args["groups"] = Group.objects.all()
 
     return render_to_response('main.html',args)
 
 def about_page(request):
     return render_to_response('about.html')
+
+def main_page_filtered(request, group = 0):
+    args = {}
+    all_good = Good.objects.filter(good_group_id=group)
+    args["current_group"] = Group.objects.get(id=group)
+    page = request.GET.get('p')
+    filtered_goods = Paginator(all_good,6)
+    args["goods"] = filtered_goods.page(page if page != None else 1)
+    args["groups"] = Group.objects.all()
+
+    return render_to_response('main.html',args)
